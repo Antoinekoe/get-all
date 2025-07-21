50e70cf2498dbbf388e1c189ea297d335f68ca31
Antoinekoe <antoine.koe@gmail.com>
2025-07-21 14:31:55 +0200

    Add route definition

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Category from "./pages/Category";
+import Contact from "./pages/Contact";
+import Page404 from "./pages/Page404";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/products" element={<Products />}></Route>
+        <Route path="/categories" element={<Category />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/*" element={<Page404 />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
