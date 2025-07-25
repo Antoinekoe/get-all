c6635ee73c97c85861746e30d428d98ac0ba729a
Antoinekoe <antoine.koe@gmail.com>
2025-07-26 00:35:06 +0200

    Add authcontext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { createContext } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userInfos, setUserInfos] = useState([]);
+
+  useEffect(() => {
+    const savedLoginStatus = localStorage.getItem("isLoggedIn");
+    if (savedLoginStatus === "true") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("isLoggedIn", isLoggedIn.toString());
+  }, [isLoggedIn]);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthContext;
